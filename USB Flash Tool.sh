d34db87adff339b3f88bd0f4b750b01bf78802c3
FilipStruhar <struhar.f@email.cz>
2024-04-24 11:31:15 +0200

    While cycle, fixed try except

--- USB Flash Tool.sh
+++ USB Flash Tool.sh
@@ -5,45 +5,52 @@ if [ "$EUID" -ne 0 ]; then
 	echo "Please run the script as root!"
   	exit 1
 else
-    BEFORE=$(ls /dev/sd*)
+
+	BEFORE=$(ls /dev/ | grep -E "sd[a-z]$")
     echo "Please insert the USB device now..."
     read -p "Press enter when ready"
-    AFTER=$(ls /dev/sd*)
+    AFTER=$(ls /dev/ | grep -E "sd[a-z]$")
+
 
     # Get difference between BEFORE and AFTER
     USB_DEVICE=$(comm -13 <(sort <<<"$BEFORE") <(sort <<<"$AFTER"))
-
+    echo $USB_DEVICE
 
     # If there's no difference let user choose manually
     # While USB_DEVICE is empty... ask
     if [[ -z "$USB_DEVICE" ]]; then
 
-        # Get a list of all connected USB disks
-        USB_DISKS=$(lsblk -o NAME,MODEL | grep 'USB DISK')
+        cycle=true
+        while $cycle
+        do
+            # Get a list of all connected USB disks
+            USB_DISKS=$(lsblk -o NAME,MODEL | grep 'USB DISK')
 
-        # Check if any USB disks are connected
-        if [[ -z "$USB_DISKS" ]]; then
-            echo "No USB disks detected. Please insert the USB and run the script again."
-            exit 1
-        fi
+            # Check if any USB disks are connected
+            if [[ -z "$USB_DISKS" ]]; then
+                echo "No USB disks detected. Please insert the USB and run the script again."
+                exit 1
+            fi
 
-        # Print the list of USB disks and prompt the user to choose one
-        echo "Connected USB disks:"
-        echo "$USB_DISKS"
-        read -p "Enter the name of the disk you want to choose: " CHOSEN_DISK
+            # Print the list of USB disks and prompt the user to choose one
+            echo "Connected USB disks:"
+            echo "$USB_DISKS"
+            read -p "Enter the name of the disk you want to choose: " CHOSEN_DISK
 
-        # Validate if chosen disk exists
-        if [[ -z $(fdisk -l | grep "$CHOSEN_DISK") ]]; then
-            echo "Non existent USB disk selected..."
-            exit 1
-        else
-            USB_DEVICE="/dev/$CHOSEN_DISK"
-        fi
+            # Validate if chosen disk exists
+            if [[ -z $(fdisk -l | grep "$CHOSEN_DISK") ]]; then
+                echo "Non existent USB disk selected..."
+            else
+                USB_DEVICE="$CHOSEN_DISK"
+                cycle=false
+            fi
+        done
+
     fi
 
     # Set other device variables
-    DEVICE_NAME=$(fdisk -l $USB_DEVICE | grep -oP 'Disk model: \K.*')
-    DEVICE_SIZE=$(df -h $USB_DEVICE | awk 'NR==2{print $2}')
+    DEVICE_NAME=$(fdisk -l /dev/$USB_DEVICE | grep -oP 'Disk model: \K.*')
+    DEVICE_SIZE=$(df -h /dev/$USB_DEVICE | awk 'NR==2{print $2}')
 
     echo "Your FLASH device has been set to: $DEVICE_NAME - $DEVICE_SIZE"
 
@@ -52,7 +59,7 @@ else
 
     echo "Your path image has been set to: $IMAGE_PATH"
 
-    echo "Burn | $IMAGE_PATH | >> | /dev/$DEVICE_NAME (/dev/$USB_DEVICE) - $DEVICE_SIZE |"
+    echo "Burn | $IMAGE_PATH | >> | $DEVICE_NAME (/dev/$USB_DEVICE) - $DEVICE_SIZE |"
     read -p "This will erase all your data on $USB_DEVICE do you wish to continue?"
 
     # Burn the .iso image onto the USB device
